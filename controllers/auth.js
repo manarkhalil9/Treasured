5822b17b2809dfa37d58b72021645a4ace3c0b38
Your Username <youremail@domain.com>
2025-11-09 20:19:54 +0300

    create a user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,30 @@
 const express = require('express')
 const router = express.Router()
 
+const bcrypt = require("bcrypt")
+const User = require('../models/user.js')
+
 router.get('/sign-up', (req, res) => {
     res.render('auth/sign-up.ejs')
+})
+
+router.post('/sign-up', async (req, res) => {
+    const userInDatabase = await User.findOne({username: req.body.username})
+    if (userInDatabase) {
+        return res.send('username already taken.')
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.send('Password and Confirm Password must match')
+    }
+
+    const hashedPassowrd = bcrypt.hashSync(req.body.password, 10)
+    req.body.password = hashedPassowrd
+
+    const user = await User.create(req.body)
+    res.send(`thanks for signing up ${user.username}`)
+
+    res.send('Form submission accepted')
 })
 
 module.exports = router
